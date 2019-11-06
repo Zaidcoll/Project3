d28ba72b5ac9eda285b27979a0723cfd6006651a
Ubuntu <ubuntu@ip-172-31-82-237.ec2.internal>
2019-11-06 10:11:06 +0000

    Added Materialize framework with testing placeholder

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template,redirect,request,url_for
 import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = os.getenv("MONGO_URI")
+
+
+mongo = PyMongo(app)
+
 @app.route('/')
+@app.route('/tasks')
 def hello():
-    return "Hello World.... again!"
+    return render_template('get_tasks.html', tasks = mongo.db.tasks.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
